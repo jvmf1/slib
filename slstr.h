951b96ae9c405e3dfbde5c72b70a2eef6807f60d
jvmf1 <jvzrzf@gmail.com>
2021-12-25 14:48:29 -0300

    add sl_str_fread comment

--- slstr.h
+++ slstr.h
@@ -81,4 +81,5 @@ void sl_str_trim_left(sl_str *str, const char ch);
 // calculate levenshtein distance
 size_t sl_str_distance (sl_str *str, sl_str * str2);
 
+// sl_str_fread does not close FILE
 sl_str* sl_str_fread(FILE *f);
